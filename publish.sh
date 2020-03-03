6444a8cced985dda1bdc23f3220b1c560834320c
Unknown <andydragon@live.com>
2020-03-02 23:11:39 -0500

    Update the public file for Windows and add one for macOS.

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
-@echo off
+#!/bin/bash
 
 dotnet publish -c Release -r win-x64 --self-contained true /p:PublishSingleFile=true
 dotnet publish -c Release -r linux-x64 --self-contained true /p:PublishSingleFile=true
